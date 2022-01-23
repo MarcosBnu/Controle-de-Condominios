a470d7509a3649e4b27cc8da45fb12047c351858
MarcosBnu <84579222+MarcosBnu@users.noreply.github.com>
2022-01-23 20:09:04 -0300

    Sistema finalizado

---
+++ application/model/modelo.py
@@ -0,0 +1,99 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, json
+from flask_cors import CORS
+import os
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SECRET_KEY']="secret"
+db= SQLAlchemy(app)
+#importa as bibliotecas necessarias 
+
+
+caminho = os.path.dirname(os.path.abspath(__file__)) 
+arquivobd = os.path.join(caminho, "banco.db") #recebe o caminho do arquivo banco.db
+
+
+
+
+
+
+class Inquilinos (db.Model):#cria a tabela Inquilinos 
+    #cria as colunas
+    id = db.Column(db.Integer, primary_key=True)#chave primaria
+    nome_inquilino=db.Column(db.String(200), nullable=False)
+    idade= db.Column(db.Integer, nullable=False)
+    sexo= db.Column(db.String(18), nullable=False)
+    telefone= db.Column(db.String(20), unique=True, nullable=False)
+    email= db.Column(db.String(200), unique=True, nullable=False)
+    #nullable significa que o campo não pode ser nulo
+    def __str__(self):
+        return f'{str(self.id)},{self.nome_inquilino}, {self.idade}, {self.sexo}, {self.telefone}, {self.email}'
+    #retorna em string as informaçoes
+    def json(self):
+        return{
+            "id" : self.id,
+            "nome_inquilino" : self.nome_inquilino,
+            "idade" : self.idade,
+            "sexo" : self.sexo,
+            "telefone" : self.telefone,
+            "email" : self.email,
+            #retorna em join as informaçoes
+        }
+        
+class Unidades (db.Model):#cria a tabela Unidades
+    #cria as colunas
+    idunidades = db.Column(db.Integer, primary_key=True)#chave primaria
+    unidade_nome=db.Column(db.String(200), nullable=False)
+    condominio=db.Column(db.String(200), nullable=False)
+    endereço= db.Column(db.String(400), unique=True, nullable=False)
+    #nullable significa que o campo não pode ser nulo
+    proprietario = db.Column(db.String(400), nullable= False)
+    def __str__(self):
+        return f'{str(self.idunidades)}, {self.unidade_nome}, {self.condominio}, {self.endereço}, {self.proprietario}'
+    #retorna em string as informaçoes
+    def json(self):
+        return{
+            "idunidades" : self.idunidades,
+            "unidade_nome" : self.unidade_nome,
+            "condominio" : self.condominio,
+            "endereço" : self.endereço,
+            "proprietario" : self.proprietario,
+            #retorna em join as informaçoes
+        }
+
+
+class Despesas(db.Model):#cria a tabela Despesas
+    #cria as colunas
+    iddespesas = db.Column(db.Integer, primary_key=True)#chave primaria
+    desc=db.Column(db.String(500), nullable=False)
+    tipo_despesa=db.Column(db.String(10), nullable=False)
+    valor= db.Column(db.Integer, nullable=False) 
+    vencimento= db.Column(db.String, nullable=False)
+    pagamento = db.Column(db.String, nullable= False)
+    
+    # relacionamento das clases
+    unidade_despesa = db.Column(db.Integer, db.ForeignKey(Unidades.idunidades), nullable= False)
+    unidades = db.relationship("Unidades", foreign_keys =  unidade_despesa)#chave estrangeira
+    #nullable significa que o campo não pode ser nulo
+    def __str__(self):
+        return f'{str(self.iddespesas)}, {self.desc}, {self.tipo_despesa}, {self.valor}, {self.vencimento}, {self.pagamento}, {self.unidade_despesa}'
+    #retorna em string as informaçoes
+    def json(self):
+        return{
+            "iddespesas" : self.iddespesas,
+            "desc" : self.desc,
+            "tipo_despesa" : self.tipo_despesa,
+            "valor" : self.valor,
+            "vencimento" : self.vencimento,
+            "pagamento" : self.pagamento,
+            "unidade_despesa": self.unidade_despesa
+            #retorna em join as informaçoes
+        }
+        
+if __name__=="__main__":#testa as classes
+    if os.path.exists(arquivobd):
+        os.remove(arquivobd)#remove o banco de dados se existir
+        print("foi")
+    db.create_all()#cria o banco de dados
